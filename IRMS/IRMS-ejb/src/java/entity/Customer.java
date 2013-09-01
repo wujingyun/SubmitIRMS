1d12f099af8dd4efd865580f5b7d678f9936fb7e
wujingyun <wujingyun1128@gmail.com>
2013-09-01 15:05:44 +0800

    IRMS

---
+++ IRMS/IRMS-ejb/src/java/entity/Customer.java
@@ -0,0 +1,170 @@
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entity;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+/**
+ *
+ * @author WU JINGYUN
+ */
+
+
+
+@Entity
+
+@NamedQueries(
+    @NamedQuery(name = "getAllCustomers", query = "SELECT c FROM Customer c ORDER BY c.customerId ASC")
+)
+
+public class Customer implements Serializable 
+{
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerId;
+    @Column(length = 32)
+    private String firstName;
+    @Column(length = 32)
+    private String lastName;
+    @Column(length = 64)
+    private String address;
+    @Column(length = 64)
+    private String email;
+    @Column(length = 16)
+    private String password;
+    @Column(length = 3)
+    private String mobilePhoneCountryCode;
+    @Column(length = 16)
+    private String mobilePhoneNumber;
+    private Integer loyaltyPointBalance;
+
+    private Timestamp registrationTimestamp;
+        
+    
+
+    
+    
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (customerId != null ? customerId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the customerId fields are not set
+        if (!(object instanceof Customer)) {
+            return false;
+        }
+        Customer other = (Customer) object;
+        if ((this.customerId == null && other.customerId != null) || (this.customerId != null && !this.customerId.equals(other.customerId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Customer[ customerId=" + customerId + " ]";
+    }
+
+    
+    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMobilePhoneCountryCode() {
+        return mobilePhoneCountryCode;
+    }
+
+    public void setMobilePhoneCountryCode(String mobilePhoneCountryCode) {
+        this.mobilePhoneCountryCode = mobilePhoneCountryCode;
+    }
+
+    public String getMobilePhoneNumber() {
+        return mobilePhoneNumber;
+    }
+
+    public void setMobilePhoneNumber(String mobilePhoneNumber) {
+        this.mobilePhoneNumber = mobilePhoneNumber;
+    }
+
+    public Integer getLoyaltyPointBalance() {
+        return loyaltyPointBalance;
+    }
+
+    public void setLoyaltyPointBalance(Integer loyaltyPointBalance) {
+        this.loyaltyPointBalance = loyaltyPointBalance;
+    }
+
+  
+    public void setRegistrationTimestamp(Timestamp registrationTimestamp) {
+        this.registrationTimestamp = registrationTimestamp;
+    }
+
+  
+}
