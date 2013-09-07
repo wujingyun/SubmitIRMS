51f0513d438058f80ba41d8ce1ea7305c23bfac9
wujingyun <wujingyun1128@gmail.com>
2013-09-07 10:10:42 +0800

    Interface

---
+++ IRMSRemoteInterface/src/ejb/testRemote.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author WU JINGYUN
+ */
+@Remote
+public interface testRemote {
+    
+}
