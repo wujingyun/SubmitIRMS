75c4668d59e1312a3babf7674df86dab3eb8a93c
paragonhao <wangxiahaosin@gmail.com>
2013-09-09 23:35:33 +0800

    xiahao updated on 7th sep

--- IRMS/IRMS-ejb/src/java/ejb/ManageCatalogBean.java
+++ IRMS/IRMS-ejb/src/java/ejb/ManageCatalogBean.java
@@ -8,6 +8,7 @@ import entity.ConciergeOrder;
 import entity.Contract;
 import entity.Hotel;
 import entity.Shop;
+import exception.ExistException;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
@@ -28,16 +29,14 @@ public class ManageCatalogBean implements ManageCatalogBeanRemote {
     Contract contractEntity;
     Shop shop;
     ConciergeOrder deliveryOrder;
-    
+    Hotel hotel;
     //find the category of the shop stored in contract
 
     public void viewTenancyMix() {
         contractEntity = new Contract();
         Map<String, Integer> m = new HashMap<String, Integer>();
         
-        
-        
-        
+       
         Query q = em.createQuery("SELECT * FROM contract c ");
         
         for (Object o : q.getResultList()) {
@@ -49,22 +48,41 @@ public class ManageCatalogBean implements ManageCatalogBeanRemote {
         }
     }
     
-    public void editStoreInfo(String storeName, String description, String storeContact) {
+    public void editStoreInfo(String storeName, String description, String storeContact,String operatingHours) {
         shop =new Shop();
         Query q = em.createQuery("SELECT * FROM shop WHERE name = : storeName");
         q.setParameter("storeName", storeName);
         shop =(Shop)q.getSingleResult();
         shop.setDescription(description);
+        shop.setContract(contractEntity);
+        shop.setOperatinghours(operatingHours);
+        em.flush();
     }
     
-    public void deliveryItem(String customerName, String customerID, 
+    public void deliveryItem(String hotelName,String customerName, String customerID, 
             String contactNumber, Integer numOfItems, String description) {
             deliveryOrder = new ConciergeOrder();
-            deliveryOrder.create(customerName, customerID, contactNumber, numOfItems, description);
+            hotel = new Hotel();
+            
+            deliveryOrder.create(hotelName,customerName, customerID, contactNumber, numOfItems, description);
             Calendar cal = Calendar.getInstance();
             deliveryOrder.setOrderTime(cal);
             deliveryOrder.setStatus("In the Shopping Mall");
             
-            
+            Query q = em.createQuery("SELECT * FROM hotel WHERE name =: hotelName");
+            q.setParameter("hotelName",hotelName);
+            hotel = (Hotel)q.getSingleResult();
+            hotel.getConciergeOrders().add(deliveryOrder);
+            em.persist(deliveryOrder);
+            em.flush();
+    }
+    
+    public boolean updateDeliveryOrder(String status, String ID) throws ExistException{
+        deliveryOrder = new ConciergeOrder();
+        deliveryOrder = em.find(ConciergeOrder.class, ID);
+        if(deliveryOrder ==null) throw new ExistException("The order has not been found");
+        deliveryOrder.setStatus(status);
+        em.flush();
+        return true;
     }
 }
